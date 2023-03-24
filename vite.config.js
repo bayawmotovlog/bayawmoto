6bb4e440c7da9005757e3872162bc52cba03ea8f
John Paul Sim <johnpaul.sim@hmrphils.com>
2023-03-25 00:31:20 +0800

    m

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bayawmoto',
+  base: '/bayawmoto/dist/',
   plugins: [vue()],
   resolve: {
     alias: {
